675ff24dd4698569f6293872d0c6d106dba9e014
James <james.a.lubin@gmail.com>
2019-02-26 14:58:07 -0500

    Add glide image loading example

--- app/src/main/java/com/example/chitchat/MainActivity.java
+++ app/src/main/java/com/example/chitchat/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.chitchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +14,12 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView exampleImage = findViewById(R.id.imageView_main_activity_example);
+        //TODO: Delete this --- Glide example
+        Glide.with(this)
+                .load(R.drawable.horse) //If app has internet permissions, you can use URL here
+                .centerCrop()
+                .into(exampleImage);
     }
 }
