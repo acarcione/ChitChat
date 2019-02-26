a9ae8576793763a6f4804bd2cb853e5a27ce8683
James <james.a.lubin@gmail.com>
2019-02-26 15:01:12 -0500

    Initial commit for word_graph branch. Empty.

--- app/src/main/java/com/example/chitchat/MainActivity.java
+++ app/src/main/java/com/example/chitchat/MainActivity.java
@@ -14,12 +14,5 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ImageView exampleImage = findViewById(R.id.imageView_main_activity_example);
-        //TODO: Delete this --- Glide example
-        Glide.with(this)
-                .load(R.drawable.horse) //If app has internet permissions, you can use URL here
-                .centerCrop()
-                .into(exampleImage);
     }
 }
